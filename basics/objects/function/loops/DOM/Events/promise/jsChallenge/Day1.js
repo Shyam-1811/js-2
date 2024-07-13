80929fb2148153f8b56ca05881e5105eada2f558
Shyam-1811 <shyamverma1811@gmail.com>
2024-07-14 00:17:50 +0530

    Day_ONe

---
+++ basics/objects/function/loops/DOM/Events/promise/jsChallenge/Day1.js
@@ -0,0 +1,40 @@
+// Task - 1
+var Number = 4;
+console.log(`The number is : ${Number}`);
+// Task - 2
+let myName = " Shyam Verma"
+console.log(`My name is ${myName}`);
+// Task - 3
+const myBoolean = true;
+console.log(`Value is ${myBoolean}`);
+// Task - 4
+// number
+let myVar = 44;
+console.log(typeof(myVar));
+// string
+let myString = "Ritik Kumar"
+console.log(typeof(myString));
+// boolean
+let myBool = false 
+console.log(typeof(myBool));
+// object
+let user = {
+username : "Ram",
+age : 33
+}
+console.log(typeof(user));
+// array
+let myArr = [1,2,3,4]
+console.log(typeof(myArr));
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(typeof(cars));
+// Task - 5
+let newVar = 45;
+console.log(` value is ${newVar}`)
+newVar =  33;
+console.log(` value after reassigning is ${newVar}`)
+// Task - 6
+const newVar1 = 44;
+console.log(` value is ${newVar1}`)
+// newVar1 =  22;
+// console.log(` value after reassigning is ${newVar1}`) gives error
